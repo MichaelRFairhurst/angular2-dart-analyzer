9499792e9c52629523de16f10b0fb21f2f36624a
Mike Fairhurst <mfairhurst@google.com>
2019-04-08 08:05:40 -0700

    Ofsetting constant visitor feedback from Andrew

--- angular_analyzer_plugin/lib/src/offsetting_constant_evaluator.dart
+++ angular_analyzer_plugin/lib/src/offsetting_constant_evaluator.dart
@@ -1,8 +1,40 @@
 import 'package:analyzer/dart/ast/ast.dart' as ast;
 import 'package:analyzer/dart/ast/token.dart';
-import 'package:analyzer/src/dart/ast/utilities.dart' as utils;
+import 'package:analyzer/src/dart/ast/utilities.dart' as ast_utils;
 
-class OffsettingConstantEvaluator extends utils.ConstantEvaluator {
+/// An AST-based constant evaluator that inserts whitespace to preserve offsets.
+///
+/// Evaluates an AST via dart constant semantics, inserting whitespace so that
+/// the result can be analyzed and offsets in that result match the offsets of
+/// the code.
+///
+/// Example:
+///   input:  'adjacent' 'strings' + 'with' + 'addition'
+///   output: 'adjacent   strings     with     addition'
+///
+/// Now we can highlight any arbitrary source range in the output as erroneous,
+/// and it will match the offset of what the user wrote.
+///
+/// Note that AST-evaluation is incomplete. Users may reference variables from
+/// the file or even other files. True constant evaluation therefore requires
+/// a resolved AST.
+///
+/// Example:
+///   const before = 'foo';
+///   ...
+///   ... template: after + 'and $before' * 3;
+///   ...
+///   const after = 'bar';
+///
+/// In this case we report [offsetsAreValid] is false at the end of evaluation.
+/// When false, it means we could not cleanly map the constant value to a string
+/// for error reporting. (in the case above, it is impossible, as the beginning
+/// of the string would have to come at the end and vice versa).
+///
+/// In theory we could produce a mapping that handles these harder cases,
+/// however, it is enough to report to users that they will not get IDE support
+/// for their design.
+class OffsettingConstantEvaluator extends ast_utils.ConstantEvaluator {
   bool offsetsAreValid = true;
   Object value;
   ast.AstNode lastUnoffsettableNode;
@@ -13,7 +45,7 @@ class OffsettingConstantEvaluator extends utils.ConstantEvaluator {
     int lastEndingOffset;
     for (final string in node.strings) {
       final value = string.accept(this);
-      if (identical(value, utils.ConstantEvaluator.NOT_A_CONSTANT)) {
+      if (identical(value, ast_utils.ConstantEvaluator.NOT_A_CONSTANT)) {
         return value;
       }
       // preserve offsets across the split by padding
@@ -31,12 +63,12 @@ class OffsettingConstantEvaluator extends utils.ConstantEvaluator {
     if (node.operator.type == TokenType.PLUS) {
       // ignore: omit_local_variable_types
       final Object leftOperand = node.leftOperand.accept(this);
-      if (identical(leftOperand, utils.ConstantEvaluator.NOT_A_CONSTANT)) {
+      if (identical(leftOperand, ast_utils.ConstantEvaluator.NOT_A_CONSTANT)) {
         return leftOperand;
       }
       // ignore: omit_local_variable_types
       final Object rightOperand = node.rightOperand.accept(this);
-      if (identical(rightOperand, utils.ConstantEvaluator.NOT_A_CONSTANT)) {
+      if (identical(rightOperand, ast_utils.ConstantEvaluator.NOT_A_CONSTANT)) {
         return rightOperand;
       }
       // numeric or {@code null}
